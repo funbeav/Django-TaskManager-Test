7ad981ddb99020e67e656817d4c359ffde3e29c1
funbeav <78388528+funbeav@users.noreply.github.com>
2021-11-15 13:14:24 +0300

    Initial commit

---
+++ taskmanager/main/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.all()
+    attrs = {
+        'title': 'Главная страница сайта',
+        'tasks': tasks,
+        'link_style_index': 'text-white'
+    }
+    return render(request, 'main/index.html', attrs)
+
+
+def about(request):
+    attrs = {
+        'title': 'Страница про нас',
+        'link_style_about': 'text-white'
+    }
+    return render(request, 'main/about.html', attrs)
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверна'
+
+    form = TaskForm()
+    attrs = {
+        'title': 'Добавление задачи',
+        'link_style_create': 'text-white',
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', attrs)
+
+
+def edit(request):
+    task_id = request.GET.get('id')
+    task = Task.objects.get(pk=task_id)
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверна'
+    else:
+        mode = request.GET.get('mode')
+        if mode == "'del'":
+            task.delete()
+            return redirect('home')
+
+    form = TaskForm(instance=task)
+    attrs = {
+        'title': 'Редактирование задачи',
+        'form': form,
+        'error': error,
+        'edit': True,
+        'task_id': task_id
+    }
+    return render(request, 'main/create.html', attrs)
